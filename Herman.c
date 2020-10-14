adf0e7ea7629f73c1c07a1f73295b7aa8b40c0ae
Herman Try <hermantrym@gmail.com>
2020-10-15 05:30:36 +0700

    Update Herman.c

--- Herman.c
+++ Herman.c
@@ -1,3 +1,13 @@
 #include "Herman.h"
 
 boolean MemFull() { return (FirstAvail == Nil); }
+
+void InitTab() {
+    address P;
+
+    for (P=IndexMin; P<IndexMax; P++)
+        Next(P) = P + 1;
+
+    Next(IndexMax) = Nil;
+    FirstAvail = IndexMin;
+}
