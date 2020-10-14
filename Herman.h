ab7ea48527114472155e60fa25c1e774ff3ff857
Herman Try <hermantrym@gmail.com>
2020-10-14 20:34:52 +0700

    Update Herman.h

--- Herman.h
+++ Herman.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "boolean.h"
+#include "bool.h"
 
 #define IndexMin 1
 #define IndexMax 100
@@ -11,14 +11,13 @@
 
 typedef int infotype;
 typedef int address;
-typedef struct
-{
+
+typedef struct {
     infotype Info;
     address Next;
 }ElmtList;
 
-typedef struct
-{
+typedef struct {
     address First;
 }List;
 
@@ -29,71 +28,47 @@ address FirstAvail;
 #define Next(P) TabElmt[P].Next
 #define Info(P) TabElmt[P].Info
 
+int NbElmt(List L);
+float Average(List L);
 
-boolean MemFull();
-
-void InitTab();
-
-void AllocTab(address *P);
-
-void DeallocTab(address P);
-
-boolean ListEmpty(List L);
-
-void CreateList(List *L);
-
+address AdrMax(List L);
+address AdrMin(List L);
 address Search(List L, infotype X);
-
-boolean FSearch(List L, address P);
-
 address SearchPrec(List L, infotype X);
 
-void InsVFirst(List *L, infotype X);
+boolean MemFull();
+boolean ListEmpty(List L);
+boolean FSearch(List L, address P);
 
+infotype Mins(List L);
+infotype Maxs(List L);
+
+List FCopyList(List L);
+List FInversList(List L);
+
+void InitTab();
+void AllocTab(address *P);
+void DeallocTab(address P);
+void CreateList(List *L);
+void InsVFirst(List *L, infotype X);
 void InsVLast(List *L, infotype X);
 
-void DelVFirst(List *L, infotype *X);
-
-void DelVLast(List *L, infotype *X);
-
 void InsertFirst(List *L, address P);
-
 void InsertAfter(List *L, address P, address Prec);
-
 void InsertLast(List *L, address P);
 
-void DelFirst(List *L, address *P);
-
+void DelAll(List *L);
 void DelP(List *L, infotype X);
-
 void DelLast(List *L, address *P);
-
+void DelFirst(List *L, address *P);
+void DelVLast(List *L, infotype *X);
+void DelVFirst(List *L, infotype *X);
 void DelAfter(List *L, address *Pdel, address Prec);
 
 void PrintInfo(List L);
-
-int NbElmt(List L);
-
-infotype Maxs(List L);
-
-address AdrMax(List L);
-
-infotype Mins(List L);
-
-address AdrMin(List L);
-
-float Average(List L);
-
-void DelAll(List *L);
-
 void InversList(List *L);
 
-List FInversList(List L);
-
 void CopyList(List *L1, List *L2);
-
-List FCopyList(List L);
-
 void CpAlokList(List Lin, List *Lout);
 void Konkat(List L1, List L2, List *L3);
 void Konkat1(List *L1, List *L2, List *L3);
